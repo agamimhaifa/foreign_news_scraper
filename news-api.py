3696cd9d269efce1a00e0ba5ae5d31a8ed844027
Omer Boehm <agamim.haifa@gmail.com>
2023-11-12 20:55:02 +0200

    updated API options

--- news-api.py
+++ news-api.py
@@ -4,38 +4,31 @@ from datetime import datetime, timedelta
 today = datetime.today().strftime('%Y-%m-%d')
 Previous_Date = datetime.today() - timedelta(days=1)
 
-# Init
-#newsapi = NewsApiClient(api_key='dc3d0bad23ca4f759b84181bf3f00941')
 
-# /v2/top-headlines
-#top_headlines = newsapi.get_top_headlines(q='bitcoin',
-#                                          sources='bbc-news,the-verge',
-#                                          category='business',
-#                                          language='en',
-#                                          country='us')
-
-# /v2/everything
-#all_articles = newsapi.get_everything(q='bitcoin',
-#                                      sources='bbc-news,the-verge',
-#                                      domains='bbc.co.uk,techcrunch.com',
-#                                      from_param=Previous_Date,
-#                                      to=today,
-#                                      language='en',
-#                                      sort_by='relevancy',
-#                                      page=2)
-
-# /v2/top-headlines/sources
-#sources = newsapi.get_sources()
-
-# Initialize the NewsApiClient with your API key
+# Initialize the NewsApiClient with agamimhaifas API key
 newsapi = NewsApiClient(api_key='dc3d0bad23ca4f759b84181bf3f00941')
 
-# Set your parameters for the news query
-country = 'il'        # Country (e.g., 'US' for United States)
-category = 'technology'  # Category (e.g., 'technology')
+# /v2/top-headlines/sources
+sources = newsapi.get_sources()
+print ('Available resources:')
 
-# Fetch top headlines
-top_headlines = newsapi.get_top_headlines(country=country, category=category)
+for s in sources['sources']:
+    print ('Available resource: ', s['id'])
+
+
+# Set your parameters for the news query
+query='palestine'
+country = 'il'        # Country (e.g., 'us' for United States)
+category = 'technology'  # Category (e.g., 'business')
+language='en'
+news_sources=['bbc-news,the-verge']
+
+# Fetch top headlines /v2/top-headlines
+top_headlines = newsapi.get_top_headlines(q=query,
+                                          sources=news_sources,
+                                          category=category,
+                                          language=language,
+                                          country=country)
 if 'articles' in top_headlines:
     for article in top_headlines['articles']:
         print(f"Title: {article['title']}")
@@ -44,17 +37,31 @@ if 'articles' in top_headlines:
 else:
     print("No articles found")
 
-# Set your parameters for the news query
+ 
+top_headlines = newsapi.get_top_headlines(q='bitcoin',
+                                          sources='bbc-news,the-verge',
+                                          category='business',
+                                          language='en',
+                                          country='us')
+
+# Set your parameters for the news query /v2/everything 
 query = 'Gaza'  # Search query for "Middle East"
 language = 'en'        # Language (e.g., 'en' for English)
-sources = newsapi.get_sources()
+
 
 for s in sources['sources']:
     # Set the number of results to 10
     page_size = 10
     source = s['id']
     # Fetch top headlines with the specified query
-    everything = newsapi.get_everything(q=query, language=language, sources=source, sort_by='publishedAt', page_size=page_size)
+    everything = newsapi.get_everything(q=query, 
+                                        language=language, 
+                                        sources=source, 
+                                        #domains='bbc.co.uk,techcrunch.com'
+                                        from_param=Previous_Date,
+                                        to=today,                                        
+                                        sort_by='relevancy',#'publishedAt', 
+                                        page_size=page_size)
 
     if 'articles' in top_headlines:
         for article in everything['articles']:
@@ -63,3 +70,5 @@ for s in sources['sources']:
             print("\n")
     else:
         print("No articles found")
+
+
