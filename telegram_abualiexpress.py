9e04b5e914ccffa2313a5253afba923f6f3b3a68
Omer Boehm <omerb@il.ibm.com>
2023-11-13 12:20:05 +0200

    read from abuali express

--- telegram_abualiexpress.py
+++ telegram_abualiexpress.py
@@ -1,6 +1,12 @@
 import configparser
+import logging
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 # Read the token from the config.ini file
 config = configparser.ConfigParser()
@@ -9,28 +15,20 @@ config.read('config.ini')
 # Get the bot token
 TOKEN = config['TelegramBot']['token']
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Hello! Use /read to get the latest messages from the group.')
 
-def read_messages(update: Update, context: CallbackContext) -> None:
-    # Replace 'your_group_chat_id' with your actual group chat ID
-    group_chat_id = -100123456789  # Use a negative sign for group chat IDs
 
-    # Get the last 10 messages from the group
-    messages = context.bot.get_chat_history(chat_id=group_chat_id, limit=10)
 
-    for message in messages:
-        update.message.reply_text(f"{message.from_user.username}: {message.text}")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
 
 def main() -> None:
-    updater = Updater(TOKEN)
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("read", read_messages))
-
-    updater.start_polling()
-    updater.idle()
+    application = ApplicationBuilder().token(TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
